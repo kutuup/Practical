4f1285b99ddf76921930c644fa189a595a4ce624
kutuup <philbateman1989@hotmail.co.uk>
2017-11-02 11:26:31 +0000

    Add files via upload

---
+++ Controls.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+
+    enum facing { left, right };
+    facing direction;
+    bool dead;
+    int health;
+    int framecounter;
+    // Use this for initialization
+    void Start() {
+        dead = false;
+        health = 100;
+        framecounter = 0;
+        direction = facing.left;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+
+        framecounter++;
+        if (framecounter > 15)
+        {
+            health--;
+            GameObject.Find("HealthBar").transform.localScale = new Vector3((float)0.01 * health, 0.025f, 0.1f);
+            if (health <= 0 && !dead)
+            {
+                health = 100;
+               //Kill();
+            }
+            framecounter = 0;
+        }
+    
+
+        if (Input.GetKey(KeyCode.A) && direction == facing.right && !dead)
+        {
+            direction = facing.left;
+            this.transform.Rotate(0, 180, 0);
+        }
+        else if (Input.GetKey(KeyCode.D) && direction == facing.left && !dead)
+        {
+            direction = facing.right;
+            this.transform.Rotate(0, 180, 0);
+        }
+
+    }
+
+    void Kill()
+    {
+        dead = true;
+        this.transform.Rotate(0, 0, 180);
+    }
+
+}
